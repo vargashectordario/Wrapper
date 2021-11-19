6056b104d9ead9f9a549f4e1eb58351eaf660c1a
hectorvargasb <hector.vargasb@cadena.com.co>
2021-11-19 09:45:41 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from fastapi.encoders import jsonable_encoder
+from requests.auth import HTTPBasicAuth
+
+import pandas as pd
+import requests
+import json
+
+class Item(BaseModel):
+    identificador:str
+    empresa:str
+    registroIndex: str
+    barrio:str
+    ciudad:str
+    direccion:str
+
+app = FastAPI()
+
+# variables lupap
+key = "b011644b562a93c21a83e1b0398eaa86b358e781"
+secret = "2f3625ae38e1557f5ba771ceb2b79d116c05482d"
+headers = {'Content-type': 'application/json', 'Accept': '*/*'}
+url = "https://batch.api.lupap.co/geocode/v2"
+
+@app.post("/clientes/")
+def getItems(items : List[Item]):
+    request = jsonable_encoder(items)
+    df = pd.DataFrame(request)
+    df['country']='co'
+    order = df[['identificador','country','ciudad','direccion']]
+    order.columns = ['id', 'country', 'city', 'address']
+    NroRegitros = len(request)
+    inicio = 0
+    fin = 24
+    i=0
+    lista = []
+    while i < NroRegitros:
+        paquete = order.iloc[inicio:fin,]
+        envio =  paquete.to_json(orient='records')
+        envio = json.loads(envio)
+        inicio += 25
+        fin += 25
+        i = fin
+        r = requests.post(url, data=json.dumps(envio),headers=headers ,auth=HTTPBasicAuth(key,secret))
+        dct = json.loads(r.text)
+        lista.append(dct)
+    
+    #respuesta = jsonable_encoder(lista)
+    return dct
+    
+    
+  
+
+
